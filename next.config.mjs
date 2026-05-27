10d443b1d0b9193898cb81c232acdf90ffe294d2
Gwizdala <dggwizda@amazon.com>
2026-05-27 17:35:58 +0100

    Add GitHub Pages deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  images: { unoptimized: true },
+  basePath: process.env.PAGES_BASE_PATH || "",
+  assetPrefix: process.env.PAGES_BASE_PATH || "",
 };
 
 export default nextConfig;
